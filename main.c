c1a5dc1172d7b4a67217fa8d0c0ca1d51ea1f54a
sawinw <sawinw.colombo@gmail.com>
2019-07-01 23:27:13 +0530

    Final

--- main.c
+++ main.c
@@ -3,6 +3,21 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    float height;
+    float width;
+    float perimeter;
+    float area;
+
+    printf("Enter the height of the rectangle: ");
+    scanf("%f", &height);
+    printf("Enter the width of the rectangle: ");
+    scanf("%f", &width);
+
+    perimeter = 2 * (height + width);
+    area = (height * width);
+
+    printf("Perimeter of rectangle = %f units \n", perimeter);
+    printf("Area of rectangle = %f units \n", area);
+
     return 0;
 }
